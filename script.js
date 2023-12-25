d07339f4fedf8bdc8978c99e3b6c35fba8d88491
Adebayo Awwal Bamidele <adebayoawwaldele@gmail.com>
2023-12-25 14:29:04 +0100

    dropdown

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var menu=document.querySelector("#menu-icons");
 var navlist=document.querySelector(".navlist");
 
 menu.onclick = () =>  {
-    menu.classList.toggle("X")
+    menu.classList.toggle("-")
     navlist.classList.toggle("open")
-
 }
